7b1d0f4500efa2a893f44ece6329bc128f3fc18d
Joel Parker Henderson <joel@joelparkerhenderson.com>
2015-01-14 12:31:31 -0700

    Improve name of binstubs

--- zshrc.d/aliases/bundle.zsh
+++ zshrc.d/aliases/bundle.zsh
@@ -1,14 +1,13 @@
 # Bundle aliases
 
 # bi => bundle install with best isolation options
-alias bi='bundle install --path vendor/bundle --binstubs vendor/bundle/bin'
-alias bi-d='RAILS_ENV=development bundle install --path vendor/bundle --binstubs vendor/bundle/bin --without ""'
-alias bi-t='RAILS_ENV=test bundle install --path vendor/bundle --binstubs vendor/bundle/bin --without ""'
-alias bi-p='RAILS_ENV=production bundle install --path vendor/bundle --binstubs vendor/bundle/bin --without development test guard jasmine quality'
+alias bi='bundle install --path vendor/bundle --binstubs vendor/bundle/binstubs'
+alias bi-d='RAILS_ENV=development bundle install --path vendor/bundle --binstubs vendor/bundle/binstubs --without ""'
+alias bi-t='RAILS_ENV=test bundle install --path vendor/bundle --binstubs vendor/bundle/binstubs --without ""'
+alias bi-p='RAILS_ENV=production bundle install --path vendor/bundle --binstubs vendor/bundle/binstubs --without development test guard jasmine quality'
 
 # bx  => bundle exec
 alias bx='bundle exec'
 alias bx-d='RAILS_ENV=development bundle exec'
 alias bx-t='RAILS_ENV=test bundle exec'
 alias bx-p='RAILS_ENV=production bundle exec'
-
