81dce1df993d06058753b6d165ae5473c9344318
Joel Parker Henderson <joel@joelparkerhenderson.com>
2016-08-31 18:27:32 -0700

    Pause chruby because it's slow

--- zshrc.d/chruby.zsh
+++ zshrc.d/chruby.zsh
@@ -1,2 +1,2 @@
-f=/usr/local/share/chruby/chruby.sh && test -r $f && source $f
-RUBIES+=(`find /opt/ruby -depth 2 2>/dev/null | grep -v current`)
+#f=/usr/local/share/chruby/chruby.sh && test -r $f && source $f
+#RUBIES+=(`find /opt/ruby -depth 2 2>/dev/null | grep -v current`)
