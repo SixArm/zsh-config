947af67ef954fd766dee388bb9e7e55f010ecf5d
Joel Parker Henderson <joel@joelparkerhenderson.com>
2019-10-16 16:55:48 -0700

    Add opt help

--- zshenv.d/settings/opt.zsh
+++ zshenv.d/settings/opt.zsh
@@ -1,2 +1,15 @@
+# Set options - customize as you like
+#
+#  * extendedglob: Treat the ‘#’, ‘~’ and ‘^’ characters as part of 
+#    patterns for filename generation, etc. (An initial unquoted ‘~’
+#    always produces named directory expansion.)
+#
+#  * nomatch: If a pattern for filename generation has no matches, print
+#    an error, instead of leaving it unchanged in the argument list. This
+#    also applies to file expansion of an initial ‘~’ or ‘=’.
+# 
+#  * notify: Report the status of background jobs immediately, rather than
+#    waiting until just before printing a prompt.
+
 setopt extendedglob nomatch notify
 
