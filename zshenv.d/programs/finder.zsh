8f90b53d7507ca5801a06130189b5a827a4d36ec
Joel Parker Henderson <joel@joelparkerhenderson.com>
2019-10-16 16:45:34 -0700

    Add egrep

--- zshenv.d/programs/finder.zsh
+++ zshenv.d/programs/finder.zsh
@@ -1,7 +1,8 @@
 # Example choices for finder:
 #
-#     rg
-#     ag
-#     grep
+#   * rg (ripgrep)
+#   * ag (the silver searcher)
+#   * egrep (enhanced grep)
+#   * grep (typical grep)
 #
-(( ${+FINDER} )) || export FINDER=$(command -v rg || command -v ag || command -v grep || "")
+(( ${+FINDER} )) || export FINDER=$( command -v rg || command -v ag || command -v egrep || command -v grep || "" )
