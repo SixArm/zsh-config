4d9a3757d52ee8d80918bbce78ae6b22c5712953
Joel Parker Henderson <joel@joelparkerhenderson.com>
2019-10-16 17:12:00 -0700

    Add default

--- zshrc.d/aliases/chrome.zsh
+++ zshrc.d/aliases/chrome.zsh
@@ -8,4 +8,4 @@
 #   * google-chrome
 #   * /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome
 #
-alias chrome=$( command -v chromium || command -v chromium-browser || command -v chrome || command -v google-chrome || command -v '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome ' )
+alias chrome=$( command -v chromium || command -v chromium-browser || command -v chrome || command -v google-chrome || command -v '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome ' || "" )
