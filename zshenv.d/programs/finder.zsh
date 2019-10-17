8a69efd3318c2f1472cd1b7e7b6fc27d7ee83846
Joel Parker Henderson <joel@joelparkerhenderson.com>
2019-10-16 17:12:13 -0700

    Add finder choices

--- zshenv.d/programs/finder.zsh
+++ zshenv.d/programs/finder.zsh
@@ -2,7 +2,10 @@
 #
 #   * rg (ripgrep)
 #   * ag (the silver searcher)
+#   * sift
+#   * ucg
+#   * pt
 #   * egrep (enhanced grep)
 #   * grep (typical grep)
 #
-(( ${+FINDER} )) || export FINDER=$( command -v rg || command -v ag || command -v egrep || command -v grep || "" )
+(( ${+FINDER} )) || export FINDER=$( command -v rg || command -v ag || command -v sift || command -v ucg || command -v pt || command -v egrep || command -v grep || "" )
