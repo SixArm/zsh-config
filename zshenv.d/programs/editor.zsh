ffada6e51d65dc56b0adf6217b5dbd2f23b32ccd
Alejandro Samarín <alejandro.samarin@gmail.com>
2019-10-17 18:06:31 +0100

    Fix typo in ALTERNATE_EDITOR

--- zshenv.d/programs/editor.zsh
+++ zshenv.d/programs/editor.zsh
@@ -6,5 +6,5 @@
 #   * emacsclient --alternate-editor="" -c "$@"
 #
 (( ${+EDITOR} )) || export EDITOR=$( command -v vim || command -v vi || "" )
-(( ${+ALTERNARTE_EDITOR} )) || export ALTERNATE_EDITOR=$( command -v vim || command -v vi || "" )
+(( ${+ALTERNATE_EDITOR} )) || export ALTERNATE_EDITOR=$( command -v vim || command -v vi || "" )
 (( ${+VISUAL} )) || export VISUAL=$( command -v vim || command -v vi || "" )
