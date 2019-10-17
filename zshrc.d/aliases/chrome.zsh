c084a9aee41466c1d1b70ec3e7da5625d9a1fbf7
Joel Parker Henderson <joel@joelparkerhenderson.com>
2019-10-16 17:06:25 -0700

    Add Chrome options

--- zshrc.d/aliases/chrome.zsh
+++ zshrc.d/aliases/chrome.zsh
@@ -1,1 +1,11 @@
-[ -e /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome ] && alias chrome='/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome '
+# Alias for Chrome browser command
+#
+# Examples:
+#
+#   * chromium
+#   * chromium-browser
+#   * chrome
+#   * google-chrome
+#   * /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome
+#
+alias chrome=$( command -v chromium || command -v chromium-browser || command -v chrome || command -v google-chrome || command -v '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome ' )
