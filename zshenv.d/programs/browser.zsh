b853d2f1202d3b23a14c2c8452da303857f267a2
Joel Parker Henderson <joel@joelparkerhenderson.com>
2019-10-16 17:01:43 -0700

    Add Chrome choices

--- zshenv.d/programs/browser.zsh
+++ zshenv.d/programs/browser.zsh
@@ -1,6 +1,15 @@
 # Example choices for browser:
 #
+#   * chromium
+#   * chromium-browser
+#   * chrome
+#   * google-chrome
 #   * w3m
 #   * lynx
-#
-(( ${+BROWSER} )) || export BROWSER=$( command -v w3m || command -v lynx || "" )
+
+(( ${+BROWSER} )) || export BROWSER=$( command -v chromium || command -v chromium-browser || command -v chrome || command -v google-chrome || command -v w3m || command -v lynx || "" )
+
+
+
+
+
