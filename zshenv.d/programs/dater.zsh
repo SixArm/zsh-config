720e3d5517e0a4b7bbc4b9ec4f60cd2b1cbf62a0
Joel Parker Henderson <joel@joelparkerhenderson.com>
2019-10-16 17:39:55 -0700

    Add DATER program

---
+++ zshenv.d/programs/dater.zsh
@@ -0,0 +1,15 @@
+# Example choices for DATER:
+#
+#   * /usr/local/opt/coreutils/libexec/gnubin/date
+#   * gdate
+#   * chrome
+#   * google-chrome
+#   * w3m
+#   * lynx
+
+# Note: the items above that use `coreutils` and `gdate` are typical of 
+# macOS `brew install coreutils`, which puts the GNU command `date` in 
+# a special location and with the name `gdate` in order to ensure the 
+# GNU command does not accidentally shadow the existing macOS `date`.
+#
+(( ${+DATER} )) || export DATER=$( command -v /usr/local/opt/coreutils/libexec/gnubin/date || command -v gdate || command -v date || "" )
